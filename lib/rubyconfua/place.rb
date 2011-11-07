6b42553567b727b84451da1ce908502c3e812fcd
Alexander Simonov <alex@simonov.me>
2011-11-07 23:24:36 +0200

    Code cleaning

--- lib/rubyconfua/place.rb
+++ lib/rubyconfua/place.rb
@@ -1,61 +1,65 @@
-require 'singleton'
+module RubyConfUA
+  class Place
 
-class RubyConfUA::Place
-  include ::Singleton
-
-  def initialize
-    @city_name    = nil
-    @hall_name    = nil
-    @hall_address = nil
-    @seats_quant  = 0
-    @place_for_cb = false
-  end
-
-  attr_reader :city_name, :hall_name, :hall_address, :seats_quant, :place_for_cb
-
-  def city(city_name)
-    @city_name = city_name
-  end
-
-  def hall(hall_name)
-    @hall_name = hall_name
-  end
-
-  def address(hall_address)
-    @hall_address = hall_address
-  end
-
-  def seats(num)
-    @seats_quant = num.to_i
-  end
-
-  def place_for_coffee_brakes(val)
-    @place_for_cb = !!val
-  end
-
-  class <<self
-    def describe(&block)
-      self.instance.instance_eval(&block)
+    def initialize
+      @city_name    = nil
+      @hall_name    = nil
+      @hall_address = nil
+      @seats_quant  = 0
+      @place_for_cb = false
     end
 
-    def city
-      self.instance.city_name
+    attr_reader :city_name, :hall_name, :hall_address, :seats_quant, :place_for_cb
+
+    def city(city_name)
+      @city_name = city_name
     end
 
-    def hall
-      self.instance.hall_name
+    def hall(hall_name)
+      @hall_name = hall_name
     end
 
-    def address
-      self.instance.hall_address
+    def address(hall_address)
+      @hall_address = hall_address
     end
 
-    def seats
-      self.instance.seats_quant
+    def seats(num)
+      @seats_quant = num.to_i
     end
 
-    def place_for_coffee_brakes
-      self.instance.place_for_cb
+    def place_for_coffee_brakes(val)
+      @place_for_cb = !!val
+    end
+
+    class <<self
+
+      def instance
+        @__instance__ ||= new
+      end
+
+      def describe(&block)
+        self.instance.instance_eval(&block)
+      end
+
+      def city
+        self.instance.city_name
+      end
+
+      def hall
+        self.instance.hall_name
+      end
+
+      def address
+        self.instance.hall_address
+      end
+
+      def seats
+        self.instance.seats_quant
+      end
+
+      def place_for_coffee_brakes
+        self.instance.place_for_cb
+      end
     end
   end
 end
--- lib/rubyconfua/speaker.rb
+++ lib/rubyconfua/speaker.rb
@@ -1,56 +1,57 @@
-require 'uri'
-
-class RubyConfUA::Speaker
-  def initialize(name, &block)
-    @speaker_details = { name: name }
-    @talk_details    = {}
-    self.instance_eval(&block)
-  end
-
-  attr_reader :speaker_details, :talk_details
-
-  def bio(text)
-    @speaker_details[:bio] = text
-  end
-
-  def github(handle)
-    if handle.match(/github\.com/)
-      uri = URI.parse(handle)
-      handle = uri.path.gsub('/', '')
+module RubyConfUA
+  class Speaker
+    def initialize(name, &block)
+      @speaker_details = { name: name }
+      @talk_details    = {}
+      self.instance_eval(&block)
     end
-    @speaker_details[:github] = handle
-  end
 
-  def twitter(handle)
-    if handle.match(/twitter\.com/)
-      uri = URI.parse(handle)
-      handle = handle.fragment ? uri.fragment.split('/').last : uri.path.gsub('/')
+    attr_reader :speaker_details, :talk_details
+
+    def bio(text)
+      @speaker_details[:bio] = text
     end
-    @speaker_details[:twitter] = handle
-  end
 
-  def site(url)
-    unless url.match(URI::regexp)
-      try_protocol = "http://#{url}"
-      url = try_protocol.match(URI::regexp) ? try_protocol : nil
+    def github(handle)
+      if handle.match(/github\.com/)
+        uri = URI.parse(handle)
+        handle = uri.path.gsub('/', '')
+      end
+      @speaker_details[:github] = handle
     end
-    @speaker_details[:site] = url if url
+
+    def twitter(handle)
+      if handle.match(/twitter\.com/)
+        uri = URI.parse(handle)
+        handle = handle.fragment ? uri.fragment.split('/').last : uri.path.gsub('/')
+      end
+      @speaker_details[:twitter] = handle
+    end
+
+    def site(url)
+      unless url.match(URI::regexp)
+        try_protocol = "http://#{url}"
+        url = try_protocol.match(URI::regexp) ? try_protocol : nil
+      end
+      @speaker_details[:site] = url if url
+    end
+
+    def talk(title)
+      @talk_details[:title] = title
+    end
+
+    def description(text)
+      @talk_details[:descr] = text || ''
+    end
+
+    def lang(lng)
+      return unless %w(en ru ruby).include?(lng.to_s)
+      @talk_details[:lang] = lng
+    end
+
+    def awesomeness
+      900 # TODO
+    end
   end
 
-  def talk(title)
-    @talk_details[:title] = title
-  end
-
-  def description(text)
-    @talk_details[:descr] = text || ''
-  end
-
-  def lang(lng)
-    return unless %w(en ru ruby).include?(lng.to_s)
-    @talk_details[:lang] = lng
-  end
-
-  def awesomeness
-    900 # TODO
-  end
 end
